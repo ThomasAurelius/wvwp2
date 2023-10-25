4e2dc4e46604f3f94f8ca4cce69eb272e9a5e192
Thomas Aurelius <teg824@hotmail.com>
2023-10-25 17:22:43 -0500

    recent

--- app/players/[playerId]/page.js
+++ app/players/[playerId]/page.js
@@ -3,16 +3,16 @@ import { getPlayerById } from "@/lib/prisma/players"
 
 
 const page = async ({params}) => {
-  
-    
-    const player = await getPlayerById(params.userId);
+      
+    const player = await getPlayerById(params.playerId);
+    console.log(player)
     
    return (
       <section className='px-4 py-4'>
          <div className="relative h-40 rounded-full">
-            <h1 className='mb-3 text-2xl font-semibold tracking-tight'>Player Details{params.userId}</h1>
-            <p className="text-xl font-bold">{player.name}</p>
-            <p className="text-sm text-stone-800">USA Water Polo #:{player.usaWPnum}</p>
+            <h1 className='mb-3 text-2xl font-semibold tracking-tight'>Player Details {params.playerId}</h1>
+            <p className="text-xl font-bold"> {player.name}</p>
+            <p className="text-sm text-stone-800">USA Water Polo #: {player.usaWPnum}</p>
             <p className="text-sm text-stone-800">Email: {player.email}</p>
             <p className="text-sm text-stone-800">Phone: {player.phone}</p>
             <p className="text-sm text-stone-800">Address: {player.street}</p>
@@ -21,11 +21,11 @@ const page = async ({params}) => {
             <p className="text-sm text-stone-800">Age: {player.age}</p>
             <p className="text-sm text-stone-800">Year: {player.year}</p>
             <p className="text-sm text-stone-800">Referred By: {player.referrer}</p>
-            <p className="text-sm text-stone-800">Is Master?{player.isMaster}</p>
-            <p className="text-sm text-stone-800">Dues Paid?{player.duesPaid}</p>
-            <p className="text-sm text-stone-800">Tournaments Paid?{player.tournPaid}</p>
-            <p className="text-sm text-stone-800">Agreed to Code Of Conduct{player.agreeCoC}</p>
-            <p className="text-sm text-stone-800">Last Updated:{player.updatedAt}</p>
+            <p className="text-sm text-stone-800">Is Master? {player.isMaster}</p>
+            <p className="text-sm text-stone-800">Dues Paid? {player.duesPaid}</p>
+            <p className="text-sm text-stone-800">Tournaments Paid? {player.tournPaid}</p>
+            <p className="text-sm text-stone-800">Agreed to Code Of Conduct? {player.agreeCoC}</p>
+            <p className="text-sm text-stone-800">Last Updated: {player.updatedAt}</p>
 {/*             look up player's user (Parent/gaurdian etc) details, link to their user profile. */}
          </div>
       </section>
