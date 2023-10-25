ea3059c74013d9f763d3efea15abf4734e9521d9
Thomas Aurelius <teg824@hotmail.com>
2023-10-25 15:53:23 -0500

    recent

--- app/api/players/[id]/route.js
+++ app/api/players/[id]/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server"
 import { main } from "../route"
 import prisma from "@/prisma"
 
-export const GET = async (req:Request, res:NextResponse) => {
+export const GET = async (req, res ) => {
    try {
       const id = req.url?.split("/players/")[1];
       await main();
-      const player = await prisma.player.findFirst({ where: { id } });
+      const player = await prisma.player.findFirst({ where: { id: id } });
       
       if (!player) {
          return NextResponse.json({message: "Player not found"}, {status: 404});
@@ -19,7 +19,7 @@ export const GET = async (req:Request, res:NextResponse) => {
    }
 }
 
-export const PUT = async (req:Request, res:NextResponse) => {
+export const PUT = async (req, res) => {
    try {
       const id = req.url?.split("/players/")[1];
       const { user, userId, usaWPnum, name, gender, age, dob, year, email, phone, street, city, state, zip, referrer, isMaster, duesPaid, tournPaid, agreeCoC} = await req.json();
@@ -38,7 +38,7 @@ export const PUT = async (req:Request, res:NextResponse) => {
    }
 }
 
-export const DELETE = async (req:Request, res:NextResponse) => {
+export const DELETE = async (req, res) => {
    try {
       const id = req.url?.split("/players/")[1];
       await main();
